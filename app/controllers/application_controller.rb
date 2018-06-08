cbe6dcec1111906eca969cc3db1399c31377170c
aatifjiwani <aatif@antigravityinvestments.com>
2018-06-08 15:56:22 -0600

    Added home page, changing design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    def page_title
+        @pg_title ||= "Haxxor News"
+    end
+    
+    helper_method :page_title
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+      @pg_title = "Home | Haxxor News"
   end
 end
