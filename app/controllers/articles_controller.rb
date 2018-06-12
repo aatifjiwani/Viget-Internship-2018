3e2a02fd4471f3bb01c36220246278102ec2f9db
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-06-12 14:50:46 -0600

    testing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,32 +1,32 @@
 class ArticlesController < ApplicationController
-	def new
-		@pg_title = "Create Post | Haxxor News"
-		@article = Article.new
-	end
+  def new
+    @pg_title = "Create Post | Haxxor News"
+    @article = Article.new
+  end
 
-	def create
-		@article = Article.new(article_param)
+  def create
+    @article = Article.new(article_param)
 
-		if @article.save
-			redirect_to :controller => 'home', :action => 'index'
-		else
-			render 'new'
-		end
-	end
+    if @article.save
+      redirect_to :controller => 'home', :action => 'index'
+    else
+      render 'new'
+    end
+  end
 
-	def show
-		@article = Article.find(params[:id])
-	end
+  def show
+    @article = Article.find(params[:id])
+  end
 
-	def destroy
-		@article = Article.find(params[:id])
-		@article.content_img.purge
-		@article.destroy
-		redirect_to :controller => 'home', :action => 'index'
-	end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.content_img.purge
+    @article.destroy
+    redirect_to :controller => 'home', :action => 'index'
+  end
 
-	private
-	def article_param
-		params.require(:article).permit(:title, :body, :content_img)
-	end
+  private
+  def article_param
+    params.require(:article).permit(:title, :body, :content_img)
+  end
 end
