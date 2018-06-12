1e2dad47f4c232911986f74da803c28c491cab9d
aatifjiwani <aatif@antigravityinvestments.com>
2018-06-12 09:43:10 -0600

    Changed create post design (not major change)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-    def page_title
-        @pg_title ||= "Haxxor News"
-    end
-    
-    helper_method :page_title
+	def page_title
+			@pg_title ||= "Haxxor News"
+	end
+
+	helper_method :page_title
 end
