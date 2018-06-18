644efebb1d4a411104261b5523b15bf9eb0a5d59
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-06-18 15:03:21 -0600

    Made migration to have user_id to be null:false

--- db/migrate/20180615172223_add_column_to_users.rb
+++ db/migrate/20180615172223_add_column_to_users.rb
@@ -2,5 +2,6 @@ class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :password_token, :string
     add_index :users, :password_token
+     change_column_null :articles, :user_id, false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2018_06_15_172223) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
