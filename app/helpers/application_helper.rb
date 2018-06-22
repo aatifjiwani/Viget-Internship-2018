607b3890dfa335055c5c188dbfe6e2b7e231d788
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-06-22 15:29:48 -0600

    Added vote buttons to comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def up_vote_links(voteable_type, voteable_id, method)
-    link_to("", votes_path(voteable_id: voteable_id, voteable_type: voteable_type, value: 1), method: method, remote: true, id: "vote-#{voteable_type}-button-up-#{voteable_id}", class: "article-vote-top")
+    link_to("", votes_path(voteable_id: voteable_id, voteable_type: voteable_type, value: 1), method: method, remote: true, id: "vote-#{voteable_type}-button-up-#{voteable_id}", class: "#{voteable_type.downcase}-vote-top")
   end
 
   def down_vote_links(voteable_type, voteable_id, method)
-    link_to("", votes_path(voteable_id: voteable_id, voteable_type: voteable_type, value: -1), method: method, remote: true, id: "vote-#{voteable_type}-button-down-#{voteable_id}", class: "article-vote-bottom")
+    link_to("", votes_path(voteable_id: voteable_id, voteable_type: voteable_type, value: -1), method: method, remote: true, id: "vote-#{voteable_type}-button-down-#{voteable_id}", class: "#{voteable_type.downcase}-vote-bottom")
   end
 end
