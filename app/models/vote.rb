5b1391410a6a4a58a31d35ad68f2d3ed16d8102c
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-06-25 14:22:17 -0600

    Added article filtering scopes and added UI elements

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,13 @@
 class Vote < ApplicationRecord
   belongs_to :voteable, polymorphic: true
   belongs_to :user
+  
+  scope :article_top_10, -> { where(voteable_type: "Article").group(:voteable_id).limit(10).sum(:value) }
+  scope :article_top_10_year, -> { where("voteable_type = 'Article' AND created_at >= ?", DateTime.now - 1.year).group(:voteable_id).sum(:value) }
+  scope :article_top_10_month, -> { where("voteable_type = 'Article' AND created_at >= ?", DateTime.now - 1.month).group(:voteable_id).sum(:value) }
+  scope :article_top_10_day, -> { where("voteable_type = 'Article' AND created_at >= ?", DateTime.now - 1.day).group(:voteable_id).sum(:value) }
+  
+  def query_sorted_hash(hash)
+    hash.sort_by {|k,v| -v}
+  end
 end
