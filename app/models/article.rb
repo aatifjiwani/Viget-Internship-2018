494efa300456babd609f8dda71a55aac2945700f
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-06-26 14:26:59 -0600

    Made suggested changes

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,6 +11,11 @@ class Article < ApplicationRecord
   
   has_many :votes, as: :voteable, dependent: :destroy
   
+  scope :article_top_10, -> { joins(:votes).group(:voteable_id).limit(10).sum(:value) }
+  scope :article_top_10_year, -> { joins(:votes).where("articles.created_at >= ?", DateTime.now - 1.year).group(:voteable_id).sum(:value) }
+  scope :article_top_10_month, -> { joins(:votes).where("articles.created_at >= ?", DateTime.now - 1.month).group(:voteable_id).sum(:value) }
+  scope :article_top_10_day, -> { joins(:votes).where("articles.created_at >= ?", DateTime.now - 1.day).group(:voteable_id).sum(:value) }
+  
   def parent_is?(user)
     if user
       user.id == self.user.id
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,12 +2,5 @@ class Vote < ApplicationRecord
   belongs_to :voteable, polymorphic: true
   belongs_to :user
   
-  scope :article_top_10, -> { where(voteable_type: "Article").group(:voteable_id).limit(10).sum(:value) }
-  scope :article_top_10_year, -> { where("voteable_type = 'Article' AND created_at >= ?", DateTime.now - 1.year).group(:voteable_id).sum(:value) }
-  scope :article_top_10_month, -> { where("voteable_type = 'Article' AND created_at >= ?", DateTime.now - 1.month).group(:voteable_id).sum(:value) }
-  scope :article_top_10_day, -> { where("voteable_type = 'Article' AND created_at >= ?", DateTime.now - 1.day).group(:voteable_id).sum(:value) }
-  
-  def query_sorted_hash(hash)
-    hash.sort_by {|k,v| -v}
-  end
+  scope :user_articles_voted, lambda{|user| where(user: user, voteable_type: "Article").pluck(:voteable_id) }
 end
