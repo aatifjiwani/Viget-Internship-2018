6d1baefb857f7cd45060f5803c34a502c2f459a6
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-06-27 14:54:05 -0600

    Added location feature specs

---
+++ spec/features/location_article_user_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'Getting the current location via IP address', type: :feature do
+  describe "getting the location when signing up" do
+    before do
+      visit new_user_path
+      fill_in 'Username:', with: 'locationuser'
+      fill_in 'Email:', with: 'locationuser@example.com'
+      fill_in 'Password:', with: 'secret'
+      fill_in 'Confirm Password:', with: 'secret'
+      page.check 'Add Current Location to Profile'
+      click_on 'Sign Up'
+      visit user_path(User.first)
+    end
+    
+    it 'saves the location of the user' do
+      expect(User.first.location.class).to be(Hash)
+      expect(page).to have_content("Huntington Beach, United States")
+    end
+  end
+  
+  describe "getting the location when posting an article" do
+    before do
+      login_user
+      visit new_article_path
+      fill_in "Title:", with: "ArticleTest"
+      fill_in "Content:", with: "This is content for this article" 
+      page.check 'Add Current Location to Article'
+      click_on 'Create'
+      visit article_path(Article.first)
+    end
+    
+    it 'saves the location of the user' do
+      expect(Article.first.location.class).to be(Hash)
+      expect(page).to have_content("Huntington Beach, United States")
+    end
+  end
+end
