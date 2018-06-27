357034cf3912d52e85de30956fec5a038a3b868d
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-06-27 09:26:22 -0600

    Rebased with master, minor edits

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -10,7 +10,7 @@ module VotesHelper
   def user_vote_articles(array_of_ids)
     array_of_ids.map! {|x| Article.find(x)}
   end
-  
+
   def user_votes(user)
     user_vote_articles(Vote.user_articles_voted(user).pluck(:voteable_id))
   end
