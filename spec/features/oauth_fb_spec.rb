a33a12ed1187586ca8cb8cda6f904b55e9f27514
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-06-29 11:35:24 -0600

    Finished oauth FB feature spec. It's finally done...

--- spec/features/oauth_fb_spec.rb
+++ spec/features/oauth_fb_spec.rb
@@ -5,11 +5,31 @@ RSpec.describe 'Signing in to Facebook with OAuth', type: :feature do
     before do
       Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
       visit new_user_path
-    end
-    
-    it 'creates a new user' do
       find('#facebook').click
+    end
+
+    it 'creates a new user' do
       expect(User.count).to be(1)
+      user = User.first
+      expect(user.username).to eq("facebookuser")
+      expect(user.email).to eq("facebook@example.com")
+    end
+
+    it "doesn't allow you to sign in natively" do
+      click_on 'Sign Out'
+      visit new_sessions_path
+      fill_in 'Username:', with: 'facebookuser'
+      fill_in 'Password:', with: 'fakepassword'
+      within('.li-submit') do
+        click_on 'Sign In'
+      end
+      expect(page).to have_content "facebookuser, please sign in using Facebook"
+    end
+
+    it 'allows you to sign in via Facebook' do
+      visit new_sessions_path
+      find('#facebook').click
+      expect(page).to have_content 'Signed in!'
     end
   end
 end
