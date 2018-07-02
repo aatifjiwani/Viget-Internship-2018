5a51e4eecabbdc323bdb5ca319e3500ce0f6ed6c
Aatif Jiwani <aatif.jiwani@gmail.com>
2018-07-02 14:32:45 -0600

    Changed FB OAuth Scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.credentials.dig(:fb_oauth_id), Rails.application.credentials.dig(:fb_oauth_secret), scope: 'email,user_birthday,user_location,user_hometown'
+  provider :facebook, Rails.application.credentials.dig(:fb_oauth_id), Rails.application.credentials.dig(:fb_oauth_secret), scope: 'email'
 
   provider :linkedin, Rails.application.credentials.dig(:li_oauth_id), Rails.application.credentials.dig(:li_oauth_secret), scope: 'r_basicprofile r_emailaddress'
 end
